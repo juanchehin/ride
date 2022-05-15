353c7654a4598bda53a799dd85f39781dfcbc676
chehin <chehinjuan@gmail.com>
2022-05-15 20:53:27 -0300

    Codigo para maps

--- ride.Android/MainActivity.cs
+++ ride.Android/MainActivity.cs
@@ -17,6 +17,7 @@ namespace ride.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+            // Xamarin.FormsGoogleMaps.Init(this,savedInstanceState);
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
---
+++ ride/Conexiones/Constantes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ride.Conexiones
+    {
+    public class Constantes
+        {
+        public const string GoogleMapsApiKey = "";
+        }
+    }
