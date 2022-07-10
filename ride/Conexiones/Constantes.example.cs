24193a3f7e6313acbe0cec11c016fe6b449672a7
chehin <chehinjuan@gmail.com>
2022-07-10 16:33:51 -0300

    firebase cliente in constantes.cs

--- ride/Conexiones/Constantes.example.cs
+++ ride/Conexiones/Constantes.example.cs
@@ -1,3 +1,4 @@
+using Firebase.Database;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,6 @@ namespace ride.Conexiones
     public class ConstantesExample  // <-- Cambiar por 'Constantes'
         {
         public const string GoogleMapsApiKey = "tuKey";
-        }
+        public static FirebaseClient firebase = new FirebaseClient("https://ride-aba50-default-rtdb.firebaseio.com/");
+    }
     }
